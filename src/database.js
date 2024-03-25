1501b12fe5a8057657745340d771332bb4712c50
Ezequiel <dacostavezequiel@gmail.com>
2024-03-25 19:05:09 -0300

    last maybe

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const { MONGODB_URI } = require("./config");
 (async () => {
   try {
     const db = await mongoose.connect(MONGODB_URI);
-    console.log("DB Connected");
+    console.log("DB Connected", db.connection.host);
   } catch (error) {
     console.error(error);
   }
