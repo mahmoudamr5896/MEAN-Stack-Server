55f37e3cc89c75f16fc58d36ccbaa788fbb2ab42
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-06-03 18:01:30 +0300

    added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/test";
 const PORT = process.env.PORT || 3001;
