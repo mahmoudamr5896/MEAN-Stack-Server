875e0ae3396fd46329945d8d1b6426bec51fc05e
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-06-07 18:23:11 +0300

    added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // const dotenv = require("dotenv");
 // dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/test";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mahmoud:123@cluster0.0qd359r.mongodb.net/loladb";
 const PORT = process.env.PORT || 3001;
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const app = require("./app");
 require("./database");
 const { PORT } = require("./config");
 
-app.listen(PORT);
-console.log("Server listening on port", PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });console.log("Server listening on port", PORT);
